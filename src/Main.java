0a0f9b2e7f2ae635133c5e5bf3df95df68b9b837
User <danilgaybeshev@mail.ru>
2024-02-01 14:20:23 +0500

    Homework5

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@ public class Main {
     public static void main(String[] args) {
         data();
     }
-    public static void data ()  {
+    public static void data () {
         try {
             UserRegistrationImpl.validateLoginPassword("Sky_pro", "any_password", "any_password");
-        } catch (WrongLoginException e) {
-            System.out.println("Логин не верен, регистрация не возможна");
-            System.out.println(e.getMessage());
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароль не верен, регистрация не возможна");
-            System.out.println(e.getMessage());
-        } catch (WrongConfirmPasswordException e) {
-            System.out.println("Пароль не подтверждён, регистрация не возможна");
+        } catch (WrongLoginException | WrongPasswordException | WrongConfirmPasswordException e) {
             System.out.println(e.getMessage());
         }
     }
